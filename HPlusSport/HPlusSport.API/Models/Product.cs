8a1be50707b966e9f731d53e52624e50d8744bff
RayBishun <ray.bishun@bitsbytes.com>
2023-02-12 00:07:07 -0500

    Added SuppressModelStateInvalidFilter option.

--- HPlusSport/HPlusSport.API/Models/Product.cs
+++ HPlusSport/HPlusSport.API/Models/Product.cs
@@ -9,7 +9,11 @@ namespace HPlusSport.API.Models
         
         [Required]
         public string Sku { get; set; } = string.Empty;
+
+        [Required]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public bool IsAvailable { get; set; }
