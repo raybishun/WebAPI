8b46d1ac652d83a2f964bf72e3385cde6084c57a
RayBishun <ray.bishun@bitsbytes.com>
2023-04-25 20:33:41 -0400

    Completed JWT Auth Ctrl.

--- JwtWebAPI/JwtWebAPI/Controllers/AuthController.cs
+++ JwtWebAPI/JwtWebAPI/Controllers/AuthController.cs
@@ -1,14 +1,26 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace JwtWebAPI.Controllers
 {
+
+    // Test with https://jwt.io/
+
     [Route("api/[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
     {
         public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         [HttpPost("register")]
         public async Task<ActionResult<User>> Register(UserDto request)
@@ -35,7 +47,30 @@ namespace JwtWebAPI.Controllers
                 return BadRequest("Password Incorrect!");
             }
 
-            return Ok("Placeholder Token");
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: cred);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
         }
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
